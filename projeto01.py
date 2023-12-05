9b5831448915e9945721ada825590bf38431ed8e
Bruno Campagnoli Soares da Costa Esposito <122655406+brunoesposito3012@users.noreply.github.com>
2023-12-04 23:49:40 -0300

    Add files via upload

---
+++ projeto01.py
@@ -0,0 +1,30 @@
+def leitura(nome_arquivo: str):
+    try:
+        lista = []
+        with open(nome_arquivo, 'r') as arquivo:
+            arquivo.seek(0)
+            for linha in arquivo:
+                linha = linha.strip()
+                if linha.isdigit():
+                    valor =  float(linha)
+                    lista.append(valor)
+            return lista
+    except Exception as e:
+        print("Houve uma falha na abertura do arquivo")
+        print(e)
+        return None
+
+def soma_valores_arquivo():
+    valores = leitura("entrada.txt")
+    print(valores)
+    return sum(valores)
+
+def maior_valor_arquivo():
+    pass
+
+def multiplica_valores_arquivo():
+    pass
+
+if __name__ == '__main__':
+    resultado = soma_valores_arquivo()
+    print(resultado)
